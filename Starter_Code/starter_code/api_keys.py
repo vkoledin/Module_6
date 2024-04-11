ed35526ba4bd615e29a836f3d52cfb6dd7ce28ad
Vladimir Koledin <vkoledin04@yahoo.com>
2024-04-11 14:54:00 -0400

    final

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8e8162ecd2b7306f308928b14125e97c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2f7113d4b5a54c1e9febed960d963e9a"
